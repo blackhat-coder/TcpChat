4b33a1b5c7f0c174dee6d4dd4814ad3f7a6ba5d0
Israel <abohisrael100@gmail.com>
2025-04-29 23:46:34 +0100

    Fix #2: enable clients enter server address on startup

--- TcpChat/TcpMessenger/Program.cs
+++ TcpChat/TcpMessenger/Program.cs
@@ -1,4 +1,5 @@
-﻿using TcpMessenger;
+﻿using System.ComponentModel.DataAnnotations;
+using TcpMessenger;
 
 Console.WriteLine("=== Console Chat Client ===");
 Console.WriteLine("Enter your name to begin chatting");
@@ -13,12 +14,28 @@ while (string.IsNullOrWhiteSpace(name))
     name = Console.ReadLine()?.Trim();
 }
 
-string host = "localhost";
-int port = 6000;
+(bool success, string? host, int port, string? errorMessage) serverValidationResult;
+string? serverAddress;
+
+while (true)
+{
+    Console.Write("Enter server address (e.g., localhost:6000): ");
+    serverAddress = Console.ReadLine()?.Trim();
+    serverValidationResult = ValidateServerAddress(serverAddress);
+
+    if (serverValidationResult.success){
+        break;
+    }
+
+    if (!string.IsNullOrEmpty(serverValidationResult.errorMessage))
+    {
+        Console.WriteLine(serverValidationResult.errorMessage);
+    }
+}
 
 try
 {
-    Messenger messenger = new Messenger(host, port, name);
+    Messenger messenger = new Messenger(serverValidationResult.host!, serverValidationResult.port, name);
     await messenger.Connect();
     messenger.SendMessages();
 }
@@ -27,4 +44,35 @@ catch (Exception ex)
     Console.WriteLine($"Fatal error: {ex.Message}");
     Console.WriteLine("Press any key to exit...");
     Console.ReadKey();
+}
+
+(bool success, string? host, int port, string? errorMessage) ValidateServerAddress(string? serverAddress)
+{
+    if (string.IsNullOrWhiteSpace(serverAddress))
+    {
+        return (false, null, 0, "Server address cannot be empty. Please enter a server address.");
+    }
+
+    try
+    {
+        string[] parts = serverAddress.Split(':');
+        if (parts.Length != 2) {
+            return (false, null, 0, "Please input server address in the format <address>:<port>.");
+        }
+
+        string host = parts[0];
+        if (!int.TryParse(parts[1], out int port)){
+            return (false, host, 0, "Invalid port number. Please enter a valid integer port.");
+        }
+
+        if (port is < 1 or > 65535){
+            return (false, host, port, "Port number must be between 1 and 65535.");
+        }
+
+        return (true, host, port, null);
+    }
+    catch (Exception) // Catching general exceptions here is okay as the more specific IndexOutOfRangeException is handled above
+    {
+        return (false, null, 0, "An unexpected error occurred while validating the server address.");
+    }
 }
